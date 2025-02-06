942f18a67654b1a3fcc136614fa0f40bba03cd67
Ahmed-Ghallab <ahghallab1@gmail.com>
2025-02-06 20:53:43 +0200

    v02

--- index.js
+++ index.js
@@ -14,13 +14,13 @@
 //     autoplay: {
 //       delay: 3000,
 //     },
-// });
+// });`
 
 // lama l user y go down
 window.onscroll = function () {
   scrollFunction();
 };
-//3lshan control display none or block if user move
+//3lshan control display none or block if user move for BTN
 function scrollFunction() {
   const btn = document.getElementById("goTop");
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -34,6 +34,28 @@ document.getElementById("goTop").onclick = function () {
   document.documentElement.scrollTop = 0;
 };
 // Enable tooltips everywhere from bootstrap
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-});
+// $(function () {
+//   $('[data-toggle="tooltip"]').tooltip();
+// });
+
+//3lshan control display none or block if user move for top NAV
+function scrollFunction() {
+  const div = document.getElementById("header-top");
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    div.style.display = "none";
+  } else {
+    div.style.display = "block";
+  }
+}
+
+var Swiper = new Swiper(".swiper",{
+  spaceBetween:80,
+  loop: true,
+  speed: 6000,
+  slidesPerView:"auto",
+  centeredSlides: true,
+  freeMode: true,
+  autoplay:{
+    delay:0,
+  },
+})
