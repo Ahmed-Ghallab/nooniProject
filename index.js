da7cd90c0dedfe438aebb32fc80ec356acdf0e03
Ahmed-Ghallab <ahghallab1@gmail.com>
2025-02-07 16:05:21 +0200

    v03

--- index.js
+++ index.js
@@ -23,10 +23,17 @@ window.onscroll = function () {
 //3lshan control display none or block if user move for BTN
 function scrollFunction() {
   const btn = document.getElementById("goTop");
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.documentElement.scrollTop > 20) {
     btn.style.display = "block";
   } else {
     btn.style.display = "none";
+  }
+
+  const div = document.getElementById("header-top");
+  if ( document.documentElement.scrollTop > 1) {
+    div.style.display = "none";
+  } else {
+    div.style.display = "block";
   }
 }
 // lama a click 3la l button
@@ -37,16 +44,6 @@ document.getElementById("goTop").onclick = function () {
 // $(function () {
 //   $('[data-toggle="tooltip"]').tooltip();
 // });
-
-//3lshan control display none or block if user move for top NAV
-function scrollFunction() {
-  const div = document.getElementById("header-top");
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-    div.style.display = "none";
-  } else {
-    div.style.display = "block";
-  }
-}
 
 var Swiper = new Swiper(".swiper",{
   spaceBetween:80,
